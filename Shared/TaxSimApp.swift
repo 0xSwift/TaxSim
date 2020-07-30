f9538046542793ac43ae80176207706c55bdf288
zmeriksen <zmeriksen@icloud.com>
2020-07-30 16:58:49 -0500

    Initial Commit

---
+++ Shared/TaxSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxSimApp.swift
+//  Shared
+//
+//  Created by Zach Eriksen on 7/30/20.
+//
+
+import SwiftUI
+
+@main
+struct TaxSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
